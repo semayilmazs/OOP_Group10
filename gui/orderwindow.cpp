9d1c91aefa9a1e9c4f9ee12bfa4cc7c06c7c0b45
oozgurdemirbas <deathknock00@gmail.com>
2024-12-25 23:04:35 +0300

    Update orderwindow.cpp

--- gui/orderwindow.cpp
+++ gui/orderwindow.cpp
@@ -1,29 +1,32 @@
-#ifndef ORDERWINDOW_H
-#define ORDERWINDOW_H
+#include "OrderWindow.h"
+#include "ui_OrderWindow.h"
 
-#include <QWidget>
-#include "Order.h"
-#include "Customer.h"
+OrderWindow::OrderWindow(Order* order, Customer* customer, QWidget *parent)
+    : QWidget(parent), ui(new Ui::OrderWindow), order(order), customer(customer) {
+    ui->setupUi(this);
 
-namespace Ui {
-class OrderWindow;
+    QString details = QString("Customer: %1\nOrder ID: %2\nTotal Amount: $%3")
+                        .arg(customer->getUsername())
+                        .arg(order->getOrderID())
+                        .arg(order->calculateTotal(), 0, 'f', 2);
+    ui->orderDetailsTextEdit->setText(details);
+
+    connect(ui->confirmButton, &QPushButton::clicked, this, &OrderWindow::on_confirmButton_clicked);
+    connect(ui->paymentButton, &QPushButton::clicked, this, &OrderWindow::on_paymentButton_clicked);
 }
 
-class OrderWindow : public QWidget {
-    Q_OBJECT
+OrderWindow::~OrderWindow() {
+    delete ui;
+}
 
-public:
-    explicit OrderWindow(Order* order, Customer* customer, QWidget *parent = nullptr);
-    ~OrderWindow();
+void OrderWindow::on_confirmButton_clicked() {
+    if (order->confirmOrder(true)) {
+        ui->statusLabel->setText("Order confirmed.");
+    } else {
+        ui->statusLabel->setText("Order not confirmed.");
+    }
+}
 
-private slots:
-    void on_confirmButton_clicked();
-    void on_paymentButton_clicked();
-
-private:
-    Ui::OrderWindow *ui;
-    Order* order;
-    Customer* customer;
-};
-
-#endif // ORDERWINDOW_H
+void OrderWindow::on_paymentButton_clicked() {
+    order->selectPayment();
+}
