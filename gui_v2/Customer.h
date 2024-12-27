10fadae0f4b734fb2e206e931e7fd9a0f10b10e8
siladolas <147716884+siladolas@users.noreply.github.com>
2024-12-27 23:23:41 +0300

    Update Customer.h

--- gui_v2/Customer.h
+++ gui_v2/Customer.h
@@ -9,7 +9,12 @@
 #include <QDebug>
 
 /**
+ * @class Customer
  * @brief Derived class representing a customer in the system.
+ *
+ * The Customer class extends the User class, adding specific
+ * attributes and methods related to customer functionality,
+ * such as managing card information and address details.
  */
 class Customer : public User {
 private:
@@ -24,39 +29,64 @@ public:
      */
     Customer(const QString& name, const QString& pwd);
 
-    /**
+     /**
      * @brief Gets the customer's card information.
+     *
+     * Retrieves the current card information stored for the customer.
+     *
      * @return The card information as a QString.
      */
     QString getCardInfo() const;
 
     /**
      * @brief Sets new card information for the customer.
+     *
+     * Updates the customer's card information with the provided value.
+     *
      * @param newInfo The new card information to set.
      */
     void setCardInfo(const QString& newInfo);
 
     /**
      * @brief Gets the customer's address.
+     *
+     * Retrieves the address stored for the customer.
+     *
      * @return The address as a QString.
      */
     QString getAddress() const;
 
     /**
      * @brief Sets the customer's address.
+     *
+     * Updates the address information for the customer.
+     *
      * @param add The new address to set.
      */
     void setAddress(const QString& add);
-
+    /**
+     * @brief Displays the customer's information.
+     *
+     * Logs the customer's username and address details to the console.
+     */
     void displayInfo() const;
 
-    /**
-     * @brief signup function is overriden from User class
+   /**
+     * @brief Signs up a new customer.
+     *
+     * Checks if the username and password already exist in the system.
+     * If not, adds the new customer's details to the storage.
+     *
+     * @override
      */
     void signup() override;
 
     /**
-     * @brief login function is overriden from User class
+     * @brief Logs in an existing customer.
+     *
+     * Verifies the customer's credentials against the stored data.
+     *
+     * @override
      */
     void login() override;
 };
